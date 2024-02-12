20e326cc847b521e55ca031bc7a369294fd1c792
jan02-03 <jan02-03@hotmail.com>
2024-02-12 10:31:26 +0100

    first setup

---
+++ keyboards/handwired/rp2040_lily58/config.h
@@ -0,0 +1,29 @@
+// Copyright 2024 jan (@jan02-03)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* Serial communication settings */
+#define USE_SERIAL
+// #define SOFT_SERIAL_PIN GP1
+
+#define SERIAL_USART_TX_PIN GP0     // The GPIO pin that is used split communication.
+
+#define SPLIT_HAND_PIN GP10
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
